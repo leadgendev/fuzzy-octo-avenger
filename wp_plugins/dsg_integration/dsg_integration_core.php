e894d31a6750f48eee984397a028eec6d01f034b
Eric A Mohlenhoff <eamohl@leadsanddata.net>
2014-01-27 11:28:35 -0500

    Added plugin activation hook to create SQL table

--- wp_plugins/dsg_integration/dsg_integration_core.php
+++ wp_plugins/dsg_integration/dsg_integration_core.php
@@ -7,19 +7,62 @@
 
 class DSG_Integration_Core
 {
-	public static $SESSION_TOKEN_NAME = "dsg_core_session_token";
+	/**
+	 * Some defined class constants
+	 */
+	
+	const SESSION_TOKEN_NAME = "dsg_core_session_token";
 
-	public static $SESSION_TOKEN_EXPIATION = 60 * 60 * 24 * 30;
-
+	const SESSION_TOKEN_EXPIATION = 2592000;
+	
+	const SQL_TABLE_NAME = "_dsg_core_active_sessions";
+	
+	/**
+	 * Class Singleton Instance
+	 */
 	protected static $current_instance = NULL;
+	
+	/**
+	 * Class Instance Members
+	 */
+	
+	protected $session_token;
 
-	private $session_token;
-
-	private $active_session;
-
+	protected $active_session;
+	
+	/**
+	 * Static Class Utility and Helper Functions
+	 */
+	
+	public static function create_sql_table()
+	{
+		global $wpdb;
+		
+		$table = $wpdb->prefix . self::SQL_TABLE_NAME;
+		
+		$sql = <<<SQL
+		CREATE TABLE IF NOT EXISTS $table (
+			id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+			session_token varchar(32) NOT NULL,
+			active_session text NOT NULL,
+			created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			expires timestamp NOT NULL,
+			PRIMARY KEY (id),
+			KEY session_token (session_token)
+		)
+SQL;
+		
+		$wpdb->query( $sql );
+	}
+	
+	public static function plugin_activation()
+	{
+		self::create_sql_table();
+	}
+	
 	public static function is_session_token_set()
 	{
-		if ( isset( $_COOKIE[self::$SESSION_TOKEN_NAME] )) {
+		if ( isset( $_COOKIE[self::SESSION_TOKEN_NAME])) {
 			return TRUE;
 		} else {
 			return FALSE;
@@ -28,14 +71,30 @@ class DSG_Integration_Core
 
 	public static function is_session_token_valid( $session_token )
 	{
+		/**
+		 * TODO
+		 */
 		return TRUE;
+	}
+	
+	public static function is_session_token_expired( $session_token )
+	{
+		/**
+		 * TODO
+		 */
+		return FALSE;
 	}
 
 	public static function can_session_token_be_restored()
 	{
-		if (( isset( $_GET[self::$SESSION_TOKEN_NAME] )) &&
-			( is_session_token_valid( $_GET[self::$SESSION_TOKEN_NAME] ))) {
-			return TRUE;
+		if ( isset( $_GET[self::SESSION_TOKEN_NAME])) {
+			$restored_token = $_GET[self::SESSION_TOKEN_NAME];
+			
+			if ( self::is_session_token_valid( $restored_token ) && ( ! self::is_session_token_expired( $restored_token ))) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
 		} else {
 			return FALSE;
 		}
@@ -43,85 +102,106 @@ class DSG_Integration_Core
 
 	public static function can_session_token_be_created()
 	{
-		if (( isset( $_GET['affiliate_id'] )) &&
-			( isset( $_GET['offer_id'] )) &&
-			( isset( $_GET['transaction_id'] ))) {
+		if ( isset( $_GET['transaction_id'])) {
 			return TRUE;
 		} else {
 			return FALSE;
 		}
 	}
-
-	public static function restore_session_token()
+	
+	/**
+	 * Returns the singleton instance
+	 */
+	public static function get_instance()
 	{
-		$session_token = $_GET[self::$SESSION_TOKEN_NAME];
-
-		$expiration = time() + self::$SESSION_TOKEN_EXPIRATION;
-
-		setcookie( self::$SESSION_TOKEN_NAME, $session_token, $expiration );
+		$instance = NULL;
+		
+		if ( self::$current_instance == NULL ) {
+			$instance = new DSG_Integration_Core();
+			
+			if ( self::is_session_token_set() ) {
+				$instance->continue_session_token();
+				
+				$session_token = $instance->get_session_token();
+				
+				if ( self::is_session_token_valid() && ( ! self::is_session_token_expired())) {
+					$instance->restore_active_session();
+				} else {
+					$instance = NULL;
+				}
+			} else if ( self::can_session_token_be_restored() ) {
+				$instance->restore_session_token();
+				
+				$session_token = $instance->get_session_token();
+				
+				if ( self::is_session_token_valid() && ( ! self::is_session_token_expired())) {
+					$instance->restore_active_session();
+				} else {
+					$instance = NULL;
+				}
+			} else if ( self::can_session_token_be_created() ) {
+				$instance->create_session_token();
+				$instance->create_active_session();
+			} else {
+				$instance = NULL;
+			}
+		}
+		
+		self::$current_instance = $instance;
+		
+		return self::$current_instance;
+	}
+	
+	/**
+	 * Class instance constructor (non-public)
+	 */
+	protected function __construct()
+	{
+	}
+	
+	/**
+	 * Class instance member functions
+	 */
+	
+	protected function continue_session_token()
+	{
+		$session_token = $_COOKIE[self::SESSION_TOKEN_NAME];
+		
+		$this->session_token = $session_token;
 	}
 
-	public static function create_session_token()
+	protected function restore_session_token()
+	{
+		$session_token = $_GET[self::SESSION_TOKEN_NAME];
+
+		$expiration = time() + self::SESSION_TOKEN_EXPIRATION;
+
+		setcookie( self::SESSION_TOKEN_NAME, $session_token, $expiration );
+		
+		$this->session_token = $session_token;
+	}
+
+	protected function create_session_token()
 	{
 		$session_token = $_GET['transaction_id'];
 
-		$expiration = time() + self::$SESSION_TOKEN_EXPIRATION;
+		$expiration = time() + self::SESSION_TOKEN_EXPIRATION;
 
-		setcookie( self::$SESSION_TOKEN_NAME, $session_token, $expiration );
-	}
-
-	public static function get_session_token()
-	{
-		return $_COOKIE[self::$SESSION_TOKEN_NAME];
-	}
-
-	public static function get_active_session( $session_token )
-	{
-		$active_session = array();
-
-		return $active_session;
-	}
-
-	public static function create_active_session( $session_token )
-	{
-		$active_session = array();
-
-		$active_session['session_token'] = $session_token;
-		$active_session['affiliate_id'] = $_GET['affiliate_id'];
-		$active_session['offer_id'] = $_GET['offer_id'];
-		$active_session['transaction_id'] = $_GET['transaction_id'];
-
-		return $active_session;
-	}
-
-	public static function get_instance()
-	{
-		if ( self::$current_instance == NULL ) {
-
-		}
-
-		return self::$current_instance;
-	}
-
-	protected function __construct( $session_token )
-	{
+		setcookie( self::SESSION_TOKEN_NAME, $session_token, $expiration );
+		
 		$this->session_token = $session_token;
-
 	}
-
-	public function create()
+	
+	public function get_session_token()
 	{
-
-	}
-
-	public function read()
-	{
-
-	}
-
-	public function update()
-	{
-
+		return $this->session_token;
 	}
 }
+
+/**
+ * hook onto some actions, filters, etc., etc., etc.....
+ */
+
+ register_activation_hook( __FILE__, array( 'DSG_Integration_Core', 'plugin_activation' ));
+
 ?>
